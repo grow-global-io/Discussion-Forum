4f3e4a26de2cfe791857959a8d0183e68766b288
Lokesh Sai Tamiri <119833495+Lokesh-tamiri@users.noreply.github.com>
2023-06-14 22:14:09 +0530

    Added dropdown in navbar

--- discussion-forum-frontend/src/components/NavigationBar.js
+++ discussion-forum-frontend/src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Image, Navbar } from "react-bootstrap";
+import { Card, Container, Image, Navbar } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import BrandLogo from "../assets/aco-logo.png";
@@ -12,12 +12,23 @@ const NavigationBar = () => {
   const user = useSelector((state) => state.auth.userInfo);
   const [visible, setVisible] = useState(false);
   const [toggler, setToggler] = useState(false);
-  const [togglerData,setTogglerData] = useState([])
-  useEffect(()=>{
-    fetch(Backend_URL+"post/get-data").then(data=>data.json()).then(data=>{data.sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate)); setTogglerData(data.slice(0,3))})
-  },[])
+  const [togglerData, setTogglerData] = useState([])
+  const [filteredData, setFilteredData] = useState([])
+  useEffect(() => {
+    fetch(Backend_URL + "post/get-data").then(data => data.json()).then(data => { data.sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate)); setTogglerData(data.slice(0, 3)) })
+    setFilteredData(togglerData)
+  }, [window.location])
 
-  console.log(togglerData)
+  const changeHandler = (e="") => {
+    setFilteredData(
+      togglerData.filter(each => each.postDescription.toLowerCase().includes(e.target.value.toLowerCase())|| each.composerName.toLowerCase().includes(e.target.value.toLowerCase()))
+    )
+  }
+
+  const redirectHandler = (each)=>{
+    window.location.pathname = "/post/"+each.id
+  }
+
   const userProfileImage = user.photoURL ? (
     <Image
       src={user.photoURL}
@@ -28,6 +39,7 @@ const NavigationBar = () => {
       className="rounded-circle"
       onClick={() => setVisible(!visible)}
     />
+
   ) : null;
   const navigate = useNavigate();
   return (
@@ -46,15 +58,53 @@ const NavigationBar = () => {
             </Navbar.Brand>
             <Link to="/" style={{ color: '#722282', display: 'inline' }}>National Co-Commissioning Hub</Link>
           </div>
-          <TextField variant="standard" fullWidth style={{ maxWidth: 600 }} className="mx-5 mt-3 mt-md-0" type="search" placeholder="Search..." onFocus={() => {
-            setToggler(true)
-          }} onBlur={() => {
-            setToggler(false)
-          }} />
+          <TextField variant="standard" fullWidth style={{ maxWidth: 600 }} className="mx-5 mt-3 mt-md-0" type="search"
+            onChange={(e) => changeHandler(e)}
+            placeholder="Search..." onFocus={() => {
+              setToggler(true)
+            }}  
+            
+            onBlur={()=>{
+              setTimeout(()=>{
+                setToggler(false)
+              },[200  ])
+            }}
+            />
           {
-            toggler && <div>
-              Toggled
-            </div>
+            toggler && <Card style={{
+              position: "absolute",
+              right: "50vw",
+              top: "80px",
+              padding: "10px 30px"
+            }} >
+              {filteredData.length>0? filteredData.map(each => {
+                return (
+                  <div onClick={()=>redirectHandler(each)}  style={{textDecoration:"none",color:"black", borderBottom:"1px solid rgba(0,0,0,0.25)", minWidth:"300px"}} className="d-flex justify-content-between">
+                    <div className="">
+                      <p>{each.composerName}</p>
+                      <p>{each.leadCommissioner}</p>
+                    </div>
+                    <div>
+                      <img src={each["cover-photo"]} height={50}/>
+                    </div>
+                  </div>
+                )
+              }):togglerData.map(each => {
+                return (
+                  <div onClick={()=>redirectHandler(each)}  style={{textDecoration:"none",color:"black", borderBottom:"1px solid rgba(0,0,0,0.25)", minWidth:"300px"}} className="d-flex justify-content-between">
+                    <div className="">
+                      <p>{each.composerName}</p>
+                      <p>{each.leadCommissioner}</p>
+                    </div>
+                    <div>
+                      <img src={each["cover-photo"]} height={50}/>
+                    </div>
+                  </div>
+                )
+              })
+              
+              }
+            </Card>
           }
           <Navbar.Collapse className="justify-content-end">
             {!user.id && (
@@ -72,7 +122,7 @@ const NavigationBar = () => {
 
         </Container>
       </Navbar>
-      
+
       <ProfileToggle user={user} show={visible} />
     </div>
   );
