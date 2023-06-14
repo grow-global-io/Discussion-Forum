c2a4bc6c6d1ad35319a1daf5902205720e46891e
Lokesh Sai Tamiri <119833495+Lokesh-tamiri@users.noreply.github.com>
2023-06-14 14:55:33 +0530

    Toggle added

---
+++ discussion-forum-frontend/src/components/NavToggle.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavToggle = () => {
+    return (
+        <div>
+            NavToggle
+        </div>
+    )
+}
+
+export default NavToggle
--- discussion-forum-frontend/src/components/NavigationBar.js
+++ discussion-forum-frontend/src/components/NavigationBar.js
@@ -5,12 +5,14 @@ import { Link, useNavigate } from "react-router-dom";
 import BrandLogo from "../assets/aco-logo.png";
 import styles from "../styles/Home.module.css";
 import ProfileToggle from "./ProfileToggle";
+import { TextField } from "@mui/material";
 
 const NavigationBar = () => {
   const user = useSelector((state) => state.auth.userInfo);
   const [visible, setVisible] = useState(false);
+  const [toggler, setToggler] = useState(false);
 
-  const userProfileImage = user.photoURL  ? (
+  const userProfileImage = user.photoURL ? (
     <Image
       src={user.photoURL}
       alt="user image"
@@ -21,10 +23,10 @@ const NavigationBar = () => {
       onClick={() => setVisible(!visible)}
     />
   ) : null;
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-    <>
-      <Navbar className="shadow">
+    <div>
+      <Navbar className="shadow flex-wrap">
         <Container>
           <div className="d-flex flex-column flex-sm-row align-items-sm-center">
             <Navbar.Brand>
@@ -36,28 +38,37 @@ const NavigationBar = () => {
                 />
               </Link>
             </Navbar.Brand>
-                <Link to="/" style={{color: '#722282', display: 'inline'}}>National Co-Commissioning Hub</Link>
+            <Link to="/" style={{ color: '#722282', display: 'inline' }}>National Co-Commissioning Hub</Link>
           </div>
-          
+          <TextField variant="standard" fullWidth style={{ maxWidth: 600 }} className="mx-5 mt-3 mt-md-0" type="search" placeholder="Search..." onFocus={() => {
+            setToggler(true)
+          }} onBlur={() => {
+            setToggler(false)
+          }} />
+          {
+            toggler && <div>
+              Toggled
+            </div>
+          }
           <Navbar.Collapse className="justify-content-end">
-          {!user.id && (
-          <button
-            className={styles.newPost}
-            onClick={() => {
-              navigate("/login");
-            }}
-          >
-            Login
-          </button>
-        )}
+            {!user.id && (
+              <button
+                className={styles.newPost}
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                Login
+              </button>
+            )}
             {userProfileImage}
           </Navbar.Collapse>
-      
+
         </Container>
       </Navbar>
-      <ProfileToggle user={user} show={visible} />
-    </>
+      <ProfileToggle user={user} show={visible} className="mt-3 mt-md-0" />
+    </div>
   );
 };
 
-export default NavigationBar;
+export default NavigationBar;
