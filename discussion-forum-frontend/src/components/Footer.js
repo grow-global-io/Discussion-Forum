a1391ea738962f5378b45c2244aec741c5167226
Lokesh Sai Tamiri <lokesh5g2@sasi.ac.in>
2024-01-23 15:06:10 +0530

    Update hosting cache and footer link URLs
    Refreshed the hosting cache with updated assets and source file
    fingerprints. Renamed JavaScript bundle to reflect new build versioning.
    Adjusted footer links in the frontend component to point to the updated
    About Us and Privacy Policy pages, ensuring compliance with the latest
    organizational web structure.
    
    Ref: #footer-links-update

--- discussion-forum-frontend/src/components/Footer.js
+++ discussion-forum-frontend/src/components/Footer.js
@@ -4,8 +4,8 @@ function Footer() {
     return (
         <Container style={{position: 'fixed', bottom: '0', left: '50%', translate: '-50% 0%', background: 'white', padding: '5px 0px'}}>
             <div className="footer-links">
-                <a href="https://americancomposers.org/about-aco/">About Us</a>
-                <a href="https://americancomposers.org/privacy-policy/">Privacy Policy and Disclaimer</a>
+                <a href="https://www.americancomposers.org/orchestral-commissions/about">About Us</a>
+                <a href="https://www.americancomposers.org/aco-privacy-policy">Privacy Policy and Disclaimer</a>
                 <a href="https://americancomposers.org">Parent Site</a>
             </div>
         </Container>
